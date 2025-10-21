c286248b8856a581e33a2a812e22c5c6fda86569
iuryr <iuryroberto@gmail.com>
2025-10-21 11:03:50 -0300

    adds telescope with keymaps for find_files in cwd, find_files in neovim config dir and find in help. still needs to add basic setup for telescope.
    Also installed external dependencies that are not in source control: ripgrep and find-fd

---
+++ lua/config/plugins/telescope.lua
@@ -0,0 +1,17 @@
+return {
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.8',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+  },
+  config = function()
+    vim.keymap.set("n", "<space>fh", require('telescope.builtin').help_tags)  --find in help
+    vim.keymap.set("n", "<space>fd", require('telescope.builtin').find_files) --find files in cwd
+    vim.keymap.set("n", "<space>en", function()                               -- find in neovim config directory
+      require('telescope.builtin').find_files {
+        cwd = vim.fn.stdpath('config')
+      }
+    end)
+  end
+}
