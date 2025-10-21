4a896c878cc56f3ede323a5bd173ba932e3a9378
iuryr <iuryroberto@gmail.com>
2025-10-21 14:07:48 -0300

    adds telescope keybind for searching files in installed packages source code

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -6,19 +6,25 @@ return {
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
   },
   config = function()
+    --basic setup
     require('telescope').setup({
       extensions = {
         fzf = {}
       }
     })
-
     require('telescope').load_extension('fzf')
 
+    --keymaps
     vim.keymap.set("n", "<space>fh", require('telescope.builtin').help_tags)  --find in help
     vim.keymap.set("n", "<space>fd", require('telescope.builtin').find_files) --find files in cwd
     vim.keymap.set("n", "<space>en", function()                               -- find in neovim config directory
       require('telescope.builtin').find_files {
         cwd = vim.fn.stdpath('config')
+      }
+    end)
+    vim.keymap.set("n", "<space>ep", function() -- find in packages source code
+      require('telescope.builtin').find_files {
+        cwd = vim.fs.joinpath(vim.fn.stdpath('data'), 'lazy')
       }
     end)
   end
