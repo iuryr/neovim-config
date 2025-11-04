21da50d61840dd6dc2dceb80f2010489cc197a5b
iuryr <iuryroberto@gmail.com>
2025-11-04 16:40:00 -0300

    diagnostic keymaps and update time opts

--- init.lua
+++ init.lua
@@ -10,12 +10,28 @@ require("config.lsp")
 vim.lsp.enable({ 'lua', 'basedpyright', 'clangd' })
 
 ---------------------- KEYMAPS -------------------------
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>")                                     --source current file
 vim.keymap.set("n", "<leader>x", ":.lua<CR>", { desc = 'E[x]ecute Lua code in current line' })    --execute lua code in current line
 vim.keymap.set("v", "<leader>x", ":lua<CR>", { desc = 'E[x]ecute Lua code in visual selection' }) --execute lua code in visual selection
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
+-- In the next two keymap sets, we need to create a function so that we can call vim.diagnostic.jump and pass parameters
+-- to each call. The table to the right (where we define desc) is used by the set function, not by the
+-- function we are attaching to said keymap. We do not need to do this to the other keymaps because the functions
+-- are called without arguments
+vim.keymap.set('n', '[d', function() vim.diagnostic.jump({ count = 1, float = true, }) end,
+  { desc = "Go to next diagnostic message" })
+vim.keymap.set('n', ']d', function() vim.diagnostic.jump({ count = -1, float = true, }) end,
+  { desc = "Go to previous diagnostic message" })
 
 ---------------------- OPTIONS -------------------------
 vim.opt.clipboard = "unnamedplus"
+-- Decrease update time
+vim.o.updatetime = 250
+vim.o.timeout = true
+vim.o.timeoutlen = 300
+
 vim.diagnostic.config({ virtual_text = true }) --lsp warnings for the whole buffer
 
 
